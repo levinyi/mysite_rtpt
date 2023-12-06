242c2686100a53a2275d9e34edc37f84107196e6
zsl503 <zsl503503@hotmail.com>
2023-12-06 21:37:03 +0800

    1. 修复user_center中中文文件map下载名称不正常的bug 2. 完成大部分super_manage的前后端，剩余部分文件操作

--- super_manage/views.py
+++ super_manage/views.py
@@ -4,50 +4,97 @@ from math import ceil
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import render
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_GET, require_POST
 
+from product.models import Vector
 from user_center.models import OrderInfo
+from user_center.views import vector_download as uc_vector_download
 
-def get_table_context(status=None):
-    if status is None:
-        orders = OrderInfo.objects
-    else:
-        orders = OrderInfo.objects.filter(status=status)
 
-    cnt_all = orders.count()
-    max_row = 10
-    if max_row * 10 > cnt_all:
-        datas = orders.order_by('order_time')
-    else:
-        datas = orders.order_by('order_time')[:max_row * 10]
-    rows = []
-    for data in datas:
-        rows.append({
+def get_table_context(table_name, status=None, start=0):
+    def vector_line(data):
+        return {
+            'id': data.id,
+            'Custom': data.user.username if data.user is not None else '',
+            "File": {},
+            "Map": 'vector_download/{}/map'.format(data.id) if data.vector_file else '',
+            "Map seq": data.vector_map,
+            "Vector ID": data.vector_id,
+            "iD20": data.id20,
+            "iU20": data.iu20
+        }
+
+    def order_line(data):
+        return {
             'id': data.id,
             'inquiry_id': data.inquiry_id if data.inquiry_id is not None else 'null',
             "custom": data.user.username,
-            "quantity": data.quantity,
+            "quantity": data.gene_infos.count(),
             "create": data.order_time.strftime('%d/%m/%Y %H:%M:%S'),
             "modify": "",
             "export": {},
             "report": data.report_file.url if data.inquiry_id is not None else '',
             "status": data.status,
             "url": data.url,
-        })
+        }
+
+    if table_name == 'order':
+        obj = OrderInfo.objects
+        if status is not None:
+            obj = obj.filter(status=status)
+        order_by = 'order_time'
+        process_fun = order_line
+    elif table_name == 'vector':
+        obj = Vector.objects
+        order_by = 'create_date'
+        process_fun = vector_line
+    else:
+        return {}
+
+    cnt_all = obj.count()
+    max_row = 15
+    # 这里用来设置一次性返回的值，暂时先设一个足够大的值，以后再改
+    if start + max_row * 100 > cnt_all:
+        datas = obj.order_by(order_by)
+    else:
+        datas = obj.order_by(order_by)[start:start + max_row * 100]
+    rows = []
+    for item in datas:
+        rows.append(process_fun(item))
     return {
         'title': "Order Manage",
         'max_page': ceil(cnt_all / max_row),
-        'max_row': 10,
+        'max_row': max_row,
         'rows': json.dumps(rows)
     }
+
+
+@login_required
+@require_GET
+def vector_download(request, vector_id, file_type):
+    return uc_vector_download(request, vector_id, file_type, True)
+
+
+@login_required
+@require_POST
+def upload_report(request):
+    return JsonResponse(data={
+        'status': 'success'
+    })
+
 
 @login_required
 @require_GET
 def get_rows(request):
     status = request.GET.get('status')
+    table_name = request.GET.get('tableName')
     if status and status.lower() == 'all'.lower():
         status = None
-    return JsonResponse(data={'status': 'success', 'data':get_table_context(status)})
+    return JsonResponse(data={
+        'status': 'success',
+        'data': get_table_context(table_name=table_name, status=status)
+    })
+
 
 @login_required
 @require_GET
@@ -60,36 +107,60 @@ def change_url(request):
     except Exception as e:
         return JsonResponse(data={'status': 'error', 'message': str(e)})
 
+
+@login_required
+@require_POST
+def submit_vector_data(request):
+    post_data = request.body
+    data = json.loads(post_data)
+    row_id = data['id']
+    try:
+        Vector.objects.filter(id=row_id).update(
+            vector_map=data.get('Map seq'),
+            vector_id=data.get('Vector ID'),
+            id20=data.get('iD20'),
+            iu20=data.get('iU20')
+        )
+        return JsonResponse(data={'status': 'success'})
+    except Exception as e:
+        return JsonResponse(data={'status': 'error', 'message': str(e)})
+
+
 @login_required
 @require_GET
 def change_status(request):
     row_id = request.GET.get('id')
     opr = request.GET.get('opr')
-    order_info = OrderInfo.objects.get(id=row_id)
+    status = OrderInfo.objects.get(id=row_id).status
     if row_id is None or opr is None:
         return JsonResponse(data={'status': 'error', 'message': 'Invalid value'})
+    status_list = ['Created', 'Synthesizing', 'Shipped', 'Completed']
+    status = status[0].upper() + status[1:].lower()
+    if status == 'Pending':
+        status = 'Created'
+    if status not in status_list:
+        return JsonResponse(data={'status': 'error', 'message': 'Backstage error'})
+    index = status_list.index(status)
     if opr == 'revoke':
-        pass
+        if index == 0:
+            return JsonResponse(data={'status': 'failed', 'message': "Can't revoke the {} status".format(status)})
+        OrderInfo.objects.filter(id=row_id).update(status=status_list[index - 1])
     elif opr == 'next':
-        pass
-    status = 'CREATED'
-    return JsonResponse(data={'status': 'success', 'message': 'Change success', 'newVal': status})
+        if index == len(status_list) - 1:
+            return JsonResponse(data={'status': 'failed', 'message': "Can't next the {} status".format(status)})
+        OrderInfo.objects.filter(id=row_id).update(status=status_list[index + 1])
+    return JsonResponse(data={
+        'status': 'success',
+        'message': 'Change success',
+        'newVal': OrderInfo.objects.get(id=row_id).status
+    })
+
 
 @login_required
 def order_manage(request):
-    return render(request, 'super_manage/order_manage.html', get_table_context())
+    return render(request, 'super_manage/order_manage.html', get_table_context("order"))
 
 
 @login_required
 def vector_manage(request):
-    # cart = Cart.objects.get(user=request.user)
-    # shopping_cart = cart.genes.all()
-    # page_object = Pagination(request, shopping_cart)
-    # context = {
-    #     'shopping_cart': page_object.page_queryset,  # 分完页的数据
-    #     'page_string':page_object.html(),  # 页码
-    # }
-    context = {
-        'shopping_cart': "shopping_cart",
-    }
-    return render(request, 'super_manage/vector_manage.html', context)
+    return render(request, 'super_manage/vector_manage.html', get_table_context("vector"))
